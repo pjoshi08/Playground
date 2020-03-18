840229008fd4df1e7dc7493a75dfa145fc199779
Parth <joshi.1parth@gmail.com>
2020-03-18 13:27:22 +0530

    repo: switch coroutine thread for blocking calls

--- app/src/main/java/com/pj/playground/data/TitleRepository.kt
+++ app/src/main/java/com/pj/playground/data/TitleRepository.kt
@@ -3,7 +3,8 @@ package com.pj.playground.data
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.pj.playground.utils.BACKGROUND
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -61,8 +62,27 @@ class TitleRepository(val network: Network, val titleDao: TitleDao) {
     }
 
     suspend fun refreshTitle() {
-        // TODO: Refresh from network and write to database
-        delay(500)
+        // COMPLETED: Refresh from network and write to database
+        // interact with *blocking* network and IO calls from a coroutine
+        withContext(Dispatchers.IO) {
+            val result = try {
+                // Make network request using a blocking call
+                network.fetchNextTitle().execute()
+            } catch (cause: Throwable) {
+                // If the network throws an exception, inform the caller
+                throw TitleRefreshError("Unable to refresh title", cause)
+            }
+
+            if (result.isSuccessful) {
+                // Save it to database
+                titleDao.insertTitle(Title((result.body()!!)))
+            } else {
+                // If it's not successful, inform the caller for an error
+                throw TitleRefreshError("Unable to refresh title", null)
+            }
+
+            // Advanced Tip: We can use Coroutine Cancellation here
+        }
     }
 }
 
