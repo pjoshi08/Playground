f5524ddc74423e7b0d426e88bb6b0d5d3b404619
Parth <joshi.1parth@gmail.com>
2020-05-27 18:55:57 +0530

    ex7: handle notification in foreground

--- app/src/main/java/com/pj/playground/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/pj/playground/fcm/MyFirebaseMessagingService.kt
@@ -1,8 +1,11 @@
 package com.pj.playground.fcm
 
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.pj.playground.util.sendNotification
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -10,10 +13,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMsg: RemoteMessage) {
         super.onMessageReceived(remoteMsg)
+
+        remoteMsg.data.let {
+            Log.d(TAG, "Message data payload: ${remoteMsg.data}")
+        }
+
+        remoteMsg.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body!!)
+        }
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d(TAG, "Refreshed token: $token")
+    }
+
+    private fun sendNotification(msgBody: String) {
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.sendNotification(msgBody, applicationContext)
     }
 }
