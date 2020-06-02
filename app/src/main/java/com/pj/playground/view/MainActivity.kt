ba04bc1a2153d4be88b8e0e4bc2c2c8336c7e068
Parth <joshi.1parth@gmail.com>
2020-06-02 21:28:16 +0530

    ex1: custom canvas view, override onSizeChanged, onDraw

--- app/src/main/java/com/pj/playground/view/MainActivity.kt
+++ app/src/main/java/com/pj/playground/view/MainActivity.kt
@@ -2,12 +2,18 @@ package com.pj.playground.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import com.pj.playground.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val canvasView = MyCanvasView(this)
+        canvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        canvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(canvasView)
     }
 }
--- app/src/main/java/com/pj/playground/view/MyCanvasView.kt
+++ app/src/main/java/com/pj/playground/view/MyCanvasView.kt
@@ -1,6 +1,34 @@
 package com.pj.playground.view
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import com.pj.playground.R
 
-class MyCanvasView(context: Context) : View(context) {}
+class MyCanvasView(context: Context) : View(context) {
+
+    // Canvas, Bitmap to cache what has been drawn before
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        // Note: onSizeChanged() is called everytime the size of the view has changed and the view needs to be redrawn.
+        // If we do no recycle previously created bitmaps, this can lead to memory leaks
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+}
