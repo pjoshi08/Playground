8911229a3e4e3bf62b1eecf3ce4694bb8e970aee
Parth <joshi.1parth@gmail.com>
2021-03-04 22:37:21 +0530

    ex8: step8 completed

--- app/src/main/java/com/pj/playground/view/activity/Step8Activity.kt
+++ app/src/main/java/com/pj/playground/view/activity/Step8Activity.kt
@@ -2,11 +2,26 @@ package com.pj.playground.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.AppBarLayout
 import com.pj.playground.R
+import kotlinx.android.synthetic.main.activity_step8.*
 
 class Step8Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_step8)
+
+        coordinateMotion()
+    }
+
+    private fun coordinateMotion() {
+        // TODO: set progress of MotionLayout based on an AppBarLayout.OnOffsetChangedListener
+
+        val listener = AppBarLayout.OnOffsetChangedListener { _, verticalOffset ->
+            val seekPosition = -verticalOffset / appbar_layout.totalScrollRange.toFloat()
+            motion_layout.progress = seekPosition
+        }
+
+        appbar_layout.addOnOffsetChangedListener(listener)
     }
 }
